bcb43474453d6dc742b595124f19f38f6790440b
Evan <evankempin1@outlook.com>
2025-01-16 20:00:32 -0600

    Fun Candi things we didnt quite figure out yet

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,13 +4,23 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CANdiConfiguration;
+import com.ctre.phoenix6.configs.CANdiConfigurator;
+import com.ctre.phoenix6.configs.DigitalInputsConfigs;
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.CANdi;
+import com.ctre.phoenix6.hardware.DeviceIdentifier;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
 import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.S1CloseStateValue;
+import com.ctre.phoenix6.signals.S1FloatStateValue;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Calibrations;
@@ -23,11 +33,17 @@ public class ElevatorSubsystem extends SubsystemBase {
   private final TalonFX m_elevator1;
   private final TalonFX m_elevator2;
   private final TalonFX m_elevator3;
-  private final TalonFX m_elevator4;
+  private final TalonFX m_elevator4; 
+
+  private final TalonFX m_dummy;
+
+  private final CANdi m_CaNdi;
 
   // Creates two followers for running the motors in sync.
   private final Follower m_follower;
   private final Follower m_followerInv;
+
+  // 
 
   // Creates the class for the motion profiler.
   private final MotionMagicTorqueCurrentFOC m_motionMagicTorqueCurrentFOC;
@@ -35,20 +51,33 @@ public class ElevatorSubsystem extends SubsystemBase {
   public ElevatorSubsystem() {
 
     // Initializes the TalonFX motors for the elevator.
-    m_elevator1 = new TalonFX(Constants.ElevatorConstants.kElevator1CANID);
-    m_elevator2 = new TalonFX(Constants.ElevatorConstants.kElevator2CANID);
-    m_elevator3 = new TalonFX(Constants.ElevatorConstants.kElevator3CANID);
-    m_elevator4 = new TalonFX(Constants.ElevatorConstants.kElevator4CANID);
+    m_elevator1 = new TalonFX(Constants.ElevatorConstants.kElevator1CANID, "kachow");
+    m_elevator2 = new TalonFX(Constants.ElevatorConstants.kElevator2CANID, "kachow");
+    m_elevator3 = new TalonFX(Constants.ElevatorConstants.kElevator3CANID, "kachow");
+    m_elevator4 = new TalonFX(Constants.ElevatorConstants.kElevator4CANID, "kachow");
+
+    m_dummy = new TalonFX(45, "kachow");
+
+    m_CaNdi = new CANdi(25, "kachow");
 
     // initializes the motion magic motion profiler.
     m_motionMagicTorqueCurrentFOC = new MotionMagicTorqueCurrentFOC(0);
+    
+    HardwareLimitSwitchConfigs limitConfigs = new HardwareLimitSwitchConfigs();
+
+    CANdiConfiguration candiConfig = new CANdiConfiguration();
+
+    
 
     // Creates a configurator for the motors in this subsystem.
     TalonFXConfiguration config = new TalonFXConfiguration();
 
+    
+
     // Gravity type for this subsystem.
     config.Slot0.GravityType = GravityTypeValue.Elevator_Static;
 
+    
     // Feedforward and PID settings for the motors in this subsystem.
     config.Slot0.kG = Calibrations.ElevatorCalibrations.kElevatorkG;
     config.Slot0.kS = Calibrations.ElevatorCalibrations.kElevatorkS;
@@ -62,12 +91,22 @@ public class ElevatorSubsystem extends SubsystemBase {
     config.MotionMagic.MotionMagicAcceleration = Calibrations.ElevatorCalibrations.kMaxAccelerationMotionMagic;
     config.TorqueCurrent.PeakForwardTorqueCurrent = Calibrations.ElevatorCalibrations.kMaxElevatorCurrentPerMotor;
     config.TorqueCurrent.PeakReverseTorqueCurrent = -Calibrations.ElevatorCalibrations.kMaxElevatorCurrentPerMotor;
+
+    limitConfigs.ForwardLimitSource = ForwardLimitSourceValue.RemoteCANdiS1;
+    limitConfigs.ForwardLimitRemoteSensorID = m_CaNdi.getDeviceID();
+
+    candiConfig.DigitalInputs.S1CloseState = S1CloseStateValue.CloseWhenLow;
+    candiConfig.DigitalInputs.S1FloatState = S1FloatStateValue.PullHigh;
     
     // Applies the configs to all the motors in this subsystem.
     m_elevator1.getConfigurator().apply(config.Slot0);  
     m_elevator2.getConfigurator().apply(config.Slot0);
     m_elevator3.getConfigurator().apply(config.Slot0);
     m_elevator4.getConfigurator().apply(config.Slot0);
+
+    m_CaNdi.getConfigurator().apply(candiConfig);
+
+    m_dummy.getConfigurator().apply(limitConfigs);
 
     // Declares elevator1 as lead motor. Other motors are set to follow.
     m_follower = new Follower(Constants.ElevatorConstants.kElevator1CANID, false);
@@ -94,6 +133,8 @@ public class ElevatorSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("Candy Bar", m_elevator1.getFault_ForwardHardLimit().getValue());
+    SmartDashboard.putBoolean("getName()", m_elevator1.getFault_MissingHardLimitRemote().getValue());
   }
 
   @Override
