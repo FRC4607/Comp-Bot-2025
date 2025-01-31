1e2487f6e9d2f97a52ebc10097ae42e5171762bd
cowpow0987 <evankempin1@outlook.com>
2025-01-31 15:23:59 -0600

    Changed setFieldCentric to Y

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -69,7 +69,7 @@ public class RobotContainer {
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // reset the field-centric heading on left bumper press
-        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        joystick.y().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         // Basic elevator controls
         //joystick.povDown().onTrue(new setElevatorSpeed(-0.05, m_elevator));
