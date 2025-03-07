3bda95b8008bf71bf12c4ba5e1b8146faa62605c
Cowpow0987 <evankempin1@outlook.com>
2025-03-07 15:24:28 -0600

    updated some LED stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -98,7 +98,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledPeriodic() {
-    if (m_elevator.getCANdiState()){
+    if (!m_elevator.getCANdiState()){
       LEDSubsystem.setError();
     }else{
       LEDSubsystem.setDisabled();
